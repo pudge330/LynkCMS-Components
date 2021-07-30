93e9d43d286e958f2d6674cb1088a8d18bd961b6
Brandon <b.garcia0429@outlook.com>
2021-07-30 01:48:30 -0400

    update(Functions): removed comments from old file

--- src/Functions/Autoload.php
+++ src/Functions/Autoload.php
@@ -1,22 +1,4 @@
 <?php
-/**
- * This file is part of the LynkCMS Components Package.
- *
- * (c) Brandon Garcia <me@bgarcia.dev>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @package LynkCMS Components
- * @subpackage Functions
- * @author Brandon Garcia <me@bgarcia.dev>
- */
-
-/**
- * Instantiate class to load functions file.
- */
-(new \lynk\Functions());
-
 //--old function namespaces
 include __DIR__ . '/FunctionsOld.php';
 include __DIR__ . '/FunctionsOld2.php';
