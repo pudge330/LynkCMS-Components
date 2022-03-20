0c7b3ec3f9115653711b09a788590365d266cfe3
Brandon <b.garcia0429@outlook.com>
2022-03-20 02:47:03 -0400

    update(command): moved Command component to new namespace

--- src2/Command/ContainerAwareCommand.php
+++ src2/Command/ContainerAwareCommand.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * This file is part of the LynkCMS Components Package.
+ * This file is part of the Lynk Components Package.
  *
  * (c) Brandon Garcia <me@bgarcia.dev>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @package LynkCMS Components
+ * @package Lynk Components
  * @subpackage Command
  * @author Brandon Garcia <me@bgarcia.dev>
  */
 
-namespace LynkCMS\Component\Command;
+namespace Lynk\Component\Command;
 
 use LynkCMS\Component\Container\ContainerAwareTrait;
 use Symfony\Component\Console\Input\InputInterface;
