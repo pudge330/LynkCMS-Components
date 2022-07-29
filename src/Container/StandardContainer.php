ff2f30fe131153b003ed23ccc064042cbba172c2
Brandon <b.garcia0429@outlook.com>
2022-07-28 21:22:35 -0400

    Update/normalized/fixed doc-block comments, refactored some methods

--- src/Container/StandardContainer.php
+++ src/Container/StandardContainer.php
@@ -20,12 +20,12 @@ use Symfony\Component\Yaml\Yaml;
 class StandardContainer {
 
 	/**
-	 * @var Array The data array.
+	 * @var array The data array
 	 */
 	protected $data;
 
 	/**
-	 * @para Array $data Optional. Data array to store.
+	 * @param array|object|StandardContainer $data Optional. Data array to store
 	 */
 	public function __construct($data = []) {
 		$this->data = [];
@@ -35,33 +35,33 @@ class StandardContainer {
 	/**
 	 * PHP magic get method to get a value by key using the object property syntax.
 	 * 
-	 * @param string $key The values key.
+	 * @param string $key The values key
 	 * 
-	 * @return mixed The value or null if not set.
+	 * @return mixed The value or null if not set
 	 */
-	public function __get($key) {
+	public function __get(string $key) {
 		return $this->get($key);
 	}
 
 	/**
 	 * PHP magic method to set a value to a key using the object property syntax.
 	 * 
-	 * @param string $key The key for the value.
-	 * @param mixed $value The value to store.
+	 * @param string $key The key for the value
+	 * @param mixed $value The value to store
 	 */
-	public function __set($key, $value) {
+	public function __set(string $key, $value) {
 		$this->set($key, $value);
 	}
 
 	/**
 	 * PHP magic method to call a method stored in the global array.
 	 * 
-	 * @param string $method The methods key name.
-	 * @param Array $args Optional. The parameters to call the method with as an array.
+	 * @param string $method The methods key name
+	 * @param array $args Optional. The parameters to call the method with as an array
 	 * 
-	 * @return mixed The returned value from the closure call.
+	 * @return mixed The returned value from the closure call
 	 */
-	public function __call($method, $args) {
+	public function __call(string $method, $args) {
 		if ($this->has($method)) {
 			$func = $this->get($method);
 			return call_user_func_array($func, $args);
@@ -73,105 +73,105 @@ class StandardContainer {
 	/**
 	 * Get value.
 	 * 
-	 * @param string $key The values key.
+	 * @param string $key The values key
 	 * 
-	 * @return mixed The stored value or null if non-existent.
+	 * @return mixed The stored value or null if non-existent
 	 */
-	public function get($key) {
-		if ($this->has($key))
-			return $this->data[$key];
-		else
-			return null;
+	public function get(string $key) {
+		return $this->has($key) ? $this->data[$key] : null;
 	}
 
 	/**
 	 * Set value by key.
 	 * 
-	 * @param string $key The values key.
-	 * @param mixed $value The value to store.
+	 * @param string $key The values key
+	 * @param mixed $value The value to store
 	 */
-	public function set($key, $value) {
+	public function set(string $key, $value) {
 		$this->data[$key] = $value;
 	}
 
 	/**
 	 * Merge data into existing array.
 	 * 
-	 * @param Array|Object|StandardContainer $data The data to merge.
+	 * @param array|object|StandardContainer $data The data to merge
+	 * @param bool $after Merge data into existing array
 	 */
-	public function merge($data) {
-		if (is_array($data)) {
-			$this->data = array_merge($this->export(), $data);
+	public function merge($data, $after = false) {
+		if ($data instanceof StandardContainer) {
+			$data = $data->export();
 		}
-		else if ($data instanceof StandardContainer) {
-			$this->data = array_merge($this->export(), $data->export());
-		}
-		else if (is_object($data)) {
-			foreach ($data as $dkey => $dval) {
-				$this->set($dkey, $dval);
+		if (is_object($data)) {
+			$array = [];
+			foreach ($data as $key => $value) {
+				$array[$key] = $value;
 			}
+			$data = $array;
 		}
+		$this->data = $after ? array_merge($this->data, $data) : array_merge($data, $this->data);
 	}
 
 	/**
 	 * Check whether or not a key exists.
 	 * 
-	 * @param string $key The key to check for.
+	 * @param string $key The key to check for
 	 * 
-	 * @return bool True if key exists, false if not.
+	 * @return bool True if key exists, false if not
 	 */
-	public function has($key) {
-		return (array_key_exists($key, $this->data));
+	public function has(string $key) {
+		return array_key_exists($key, $this->data);
 	}
 
 	/**
 	 *  Remove a value.
 	 * 
-	 * @param string $key The key of the value to remove.
+	 * @param string|array $key The key of the value to remove
 	 */
 	public function remove($key) {
-		unset($this->data[$key]);
+		$key = is_array($key) ? $key : [$key];
+		foreach ($key as $k) {
+			unset($this->data[$k]);
+		}
+	}
+
+	/**
+	 * Clear data in array.
+	 */
+	public function clear() {
+		$this->data = [];
 	}
 
 	/**
 	 * Remove all data in array.
+	 * 
+	 * @deprecated #1819a39 Use clear()
 	 */
 	public function removeAll() {
-		$this->data = [];
+		$this->clear()
 	}
 
 	/**
 	 * Check if value is null.
 	 * 
-	 * @param string $key The values key to ckeck.
+	 * @param string $key The values key to ckeck
 	 * 
-	 * @return bool true if value is null or non-existent, false otherwise.
+	 * @return bool true if value is null or non-existent, false otherwise
 	 */
-	public function isNull($key) {
-		if ($this->has($key)) {
-			if (is_null($this->get($key)))
-				return true;
-			else
-				return false;
-		}
-		else
-			return true;
+	public function isNull(string $key) {
+		return $this->has($key) ? is_null($this->get($key)) : true;
 	}
 
 	/**
 	 * Check if value is empty.
 	 * 
-	 * @param string $key The values key to check.
+	 * @param string $key The values key to check
 	 * 
-	 * @return bool True id value is null, empty or and empty string, false otherwise.
+	 * @return bool True id value is null, empty or and empty string, false otherwise
 	 */
-	public function isEmpty($key) {
+	public function isEmpty(string $key) {
 		if ($this->has($key)) {
 			$value = $this->get($key);
-			if (is_null($value) || empty($value) || $value == '')
-				return true;
-			else
-				return false;
+			return (is_null($value) || empty($value) || $value == '');
 		}
 		else
 			return true;
@@ -180,11 +180,11 @@ class StandardContainer {
 	/**
 	 * Check if a value is a closure or not.
 	 * 
-	 * @param string $key The values key.
+	 * @param string $key The values key
 	 * 
-	 * @return bool True if closure, false if not.
+	 * @return bool True if closure, false if not
 	 */
-	public function isClosure($key) {
+	public function isClosure(string $key) {
 		if ($this->has($key)) {
 			$value = $this->get($key);
 			if ($value instanceof Closure)
@@ -196,16 +196,13 @@ class StandardContainer {
 	/**
 	 * Get the data type of a stored value.
 	 * 
-	 * @param string $key The key of the value.
+	 * @param string $key The key of the value
 	 * 
-	 * @return string The data type as a string.
+	 * @return string The data type as a string
 	 */
-	public function type($key) {
+	public function type(string $key) {
 		if ($this->has($key)) {
-			$value = $this->get($key);
-			if ($value instanceof Closure)
-				return 'closure';
-			return gettype($value);
+			return $this->isClosure($key) ? 'closure' : gettype($this->get($key));
 		}
 		else
 			return 'null';
@@ -214,7 +211,7 @@ class StandardContainer {
 	/**
 	 * Run callback for each value in array. Passing in current index, key and value as parameters.
 	 * 
-	 * @param Closure $closure Callback closure.
+	 * @param Closure $closure Callback closure
 	 */
 	public function each(Closure $callback) {
 		$index = -1;
@@ -227,7 +224,7 @@ class StandardContainer {
 	/**
 	 * Export/return the array of data.
 	 * 
-	 * @return Array The array of data.
+	 * @return array The array of data
 	 */
 	public function export() {
 		return $this->data;
@@ -237,7 +234,7 @@ class StandardContainer {
 	 * Export/return the array of data.
 	 * Here for backwards compatibility.
 	 * 
-	 * @return Array The array of data.
+	 * @return array The array of data
 	 */
 	public function getData() {
 		return $this->data;
@@ -246,7 +243,7 @@ class StandardContainer {
 	/**
 	 * Export/return an array of data keys.
 	 * 
-	 * @return Array The array of keys.
+	 * @return array The array of keys
 	 */
 	public function exportKeys() {
 		return array_keys($this->data);
@@ -255,7 +252,7 @@ class StandardContainer {
 	/**
 	 * Export the data, without closures to JSON format.
 	 * 
-	 * @return string The array in JSON form.
+	 * @return string The array in JSON form
 	 */
 	public function toJSON() {
 		if (version_compare(phpversion(), '5.4', '<'))
@@ -267,7 +264,7 @@ class StandardContainer {
 	/**
 	 * Export the data, without closures to PHP serialized format.
 	 * 
-	 * @return string The array in PHP serialized form.
+	 * @return string The array in PHP serialized form
 	 */
 	public function toSerialize() {
 		return serialize($this->getDataWithoutClosures());
@@ -276,7 +273,7 @@ class StandardContainer {
 	/**
 	 * Export the data, without closures to YAML format.
 	 * 
-	 * @return string The array in YAML form.
+	 * @return string The array in YAML form
 	 */
 	public function toYAML() {
 		return Yaml::dump($this->getDataWithoutClosures(), 3);
@@ -285,11 +282,11 @@ class StandardContainer {
 	/**
 	 * Export data without closures.
 	 * 
-	 * @param StandardContainer $obj Optional. Standard container to export data from.
+	 * @param StandardContainer $obj Optional. Standard container to export data from
 	 * 
-	 * @return Array Data without closures.
+	 * @return array Data without closures
 	 */
-	public function getDataWithoutClosures($obj = null) {
+	public function getDataWithoutClosures(StandardContainer $obj = null) {
 		$obj = $obj ? $obj->export() : $this->export();
 		$arr = [];
 		foreach ($obj as $key => $val) {
@@ -303,11 +300,11 @@ class StandardContainer {
 	/**
 	 * Convert an array of keys and data to an array of StandardContainers.
 	 * 
-	 * @param Array $a An array of key array data pairs.
+	 * @param array $a An array of key array data pairs
 	 * 
-	 * @return Array The array of StandardContainers.
+	 * @return array The array of StandardContainers
 	 */
-	public static function convertArrays($a) {
+	public static function convertArrays(array $a) {
 		foreach ($a as $akey => &$aval) {
 			$aval = static::convertArray($aval);
 		}
@@ -317,9 +314,9 @@ class StandardContainer {
 	/**
 	 * Convert an array to a StandardContainer.
 	 * 
-	 * @return StandardContainer A new instance of StandardContainer.
+	 * @return StandardContainer A new instance of StandardContainer
 	 */
-	public static function convertArray($arr) {
+	public static function convertArray(array $arr) {
 		return new StandardContainer($arr);
 	}
 }
